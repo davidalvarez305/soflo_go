d0b994fcf5cd444a6e517dd17a361a76fb78b985
David Alvarez <david@southfloridaathleticclub.com>
2022-04-26 06:53:06 -0400

    boilerplate might be completed

--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/davidalvarez305/soflo_go/server/database"
+	"github.com/davidalvarez305/soflo_go/server/sessions"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
@@ -19,6 +21,8 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://localhost:4007",
 	}))
+	database.Connect()
+	sessions.Init()
 	Router(app)
 
 	app.Listen(":4007")
