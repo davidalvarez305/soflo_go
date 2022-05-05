a80595fb24f3e75cc2c20540f8f21dc5048e9307
David Alvarez <david@southfloridaathleticclub.com>
2022-05-05 08:17:27 -0400

    aws config

--- cli/server.sh
+++ cli/server.sh
@@ -36,4 +36,6 @@ apt-get install postgresql-client
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip && ./aws/install
 
-aws configure import --csv file://credentials.csv
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION
