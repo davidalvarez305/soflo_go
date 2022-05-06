622191c7e83207711a2ce9199f15726fecb70b62
David Alvarez <david@southfloridaathleticclub.com>
2022-05-06 05:58:12 -0400

    AWS configure and server setup completed

--- cli/chico.sh
+++ cli/chico.sh
@@ -61,3 +61,42 @@ Text="$(jq \
     change-zone.json)" && echo -E "${Text}" > change-zone.json
 
 aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file://change-zone.json
+
+# Update Repository
+apt-get update
+
+apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    apt-transport-https \
+    software-properties-common \
+    unzip
+
+# Docker Installation
+
+# Add Docker Official GPG Key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Add Docker Repo
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+# Install Docker Engine
+apt install docker-ce
+
+# Get Docker Compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Make File Executable
+chmod +x /usr/local/bin/docker-compose
+
+# Installation PSQL
+apt-get install postgresql-client
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip && ./aws/install
+
+# Configure AWS CLI
+scp -i $DOMAIN-Key.pem ~/.aws/credentials ubuntu@$EC2_PUBLIC_ID:~/.aws/credentials
