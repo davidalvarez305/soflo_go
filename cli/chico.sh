fbeceaebf9b855440479a18136e1821995b44a9d
David Alvarez <david@southfloridaathleticclub.com>
2022-05-07 16:55:53 -0400

    making changes to the script and testing it

--- cli/chico.sh
+++ cli/chico.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
 DATE=$(date '+%Y-%m-%d-%H-%M-%S')
-DOMAIN=$2
+DOMAIN=$2.com
 DATABASE=$4
 USER=$6
 REGION=us-east-1
 AVAILABILITY_ZONE=us-east-1a
 AMI_ID=ami-09d56f8956ab235b3
 INSTANCE_SIZE=t3a.small
-KEY_NAME="$2-Key"
-S3_BUCKET=""
-SG_GROUP_NAME="$2-SG"
+KEY_NAME=$2.pem
+S3_BUCKET=$8
 
-if [[ $1 == "-d" && $3 == "-db" && $5 == "-u" ]];
+if [[ $1 == "-d" && $3 == "-db" && $5 == "-u" && $7 == "-b" ]];
 then
+    echo "Registering domain..."
     # Register Domain
     Content="$(jq --arg domain "$DOMAIN" '.DomainName = $domain' register-domain.json)" && echo -E "${Content}" > register-domain.json
 
     aws route53domains register-domain --region $REGION --cli-input-json file://register-domain.json
 
+    echo "Creating hosted zone..."
     # Create Hosted Zone for Registered Domain
     ZONE_ID=$(aws route53 create-hosted-zone --name $DOMAIN --caller-reference $DATE | jq '.HostedZone' | grep Id | grep -Eoh "[A-Z0-9]{2,}")
+    ZONE_ID=Z09517553MH38FF1I5MCZ
 
+    echo "Changing name servers..."
     RAW_NS=$(aws route53 get-hosted-zone --id $ZONE_ID | jq '.DelegationSet' | grep -Eoh "ns-[0-9]+.awsdns-[0-9]+.[a-z]+" | cut -d " " -f 1)
 
     nameservers=""
@@ -34,89 +37,42 @@ then
     # Point Domain to Hosted Zone
     aws route53domains update-domain-nameservers --region $REGION --domain-name $DOMAIN --nameservers $nameservers
 
+    echo "Creating key pairs for EC2..."
     # Create EC2 Key Pair
-    aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > $KEY_NAME.pem
+    aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > $KEY_NAME
 
+    echo "Changing key permissions..."
     # Change Key Permissions
-    chmod 400 $KEY_NAME.pem
+    sudo chmod 400 $KEY_NAME
 
     # Upload to S3
-    aws s3 cp $KEY_NAME.pem $S3_BUCKET
+    aws s3 cp $KEY_NAME $S3_BUCKET/keys/
 
+    echo "Creating EC2 Instance..."
     # Create EC2 Instance
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type $INSTANCE_SIZE \
         --count 1 --associate-public-ip-address \
         --key-name $KEY_NAME | grep InstanceId | grep -Eoh "i-[a-z0-9]+")
+    
+    INSTANCE_ID=i-04e62e05a49443c9f
 
     # Get Instance Public Id
-    EC2_PUBLIC_ID=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | grep PublicIpAddress | greg -Eoh "[0-9.]+")
+    EC2_PUBLIC_ID=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID | grep PublicIpAddress | grep -Eoh "[0-9.]+")
 
     # Update Hosted Zone A Record to EC2 Public Id
+    echo "Updating A Record to Point to EC2 Instance..."
     Text="$(jq \
         --arg ip "$EC2_PUBLIC_ID" \
         --arg dns "$DOMAIN" \
-        '.Changes[].ResourceRecordSet.ResourceRecords = [{ Value: $ip }] | \
-        .Changes[].ResourceRecordSet.Name = $dns' \
-        change-zone.json)" && echo -E "${Text}" > change-zone.json
+        '.Changes[].ResourceRecordSet.ResourceRecords = [{ Value: $ip }] | .Changes[].ResourceRecordSet.Name = $dns' \
+        change-hosted-zone.json)" && echo -E "${Text}" > change-hosted-zone.json
 
-    aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file://change-zone.json
+    aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file://change-hosted-zone.json
 
-    # Update Repository
-    apt-get update
+    scp -r -i $KEY_NAME ./prep ubuntu@$EC2_PUBLIC_ID:/home/ubuntu/
 
-    apt-get install \
-        ca-certificates \
-        curl \
-        gnupg \
-        lsb-release \
-        apt-transport-https \
-        software-properties-common \
-        unzip
-
-    # Docker Installation
-
-    # Add Docker Official GPG Key
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-    # Add Docker Repo
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-
-    # Install Docker Engine
-    apt install docker-ce
-
-    # Get Docker Compose
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-    # Make File Executable
-    chmod +x /usr/local/bin/docker-compose
-
-    # Installation PSQL
-    apt-get install postgresql-client
-
-    # Install AWS CLI
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip && ./aws/install
-
-    # Configure AWS CLI
-    scp -i $DOMAIN-Key.pem ~/.aws/credentials ubuntu@$EC2_PUBLIC_ID:~/.aws/credentials
-
-    # Copy Env File
-    scp -i $DOMAIN-Key.pem .env ubuntu@$EC2_PUBLIC_ID:~/soflo_go/server
-
-    # Append DB Name to Env File
-    echo "POSTGRES_DB=$DATABASE" >> .env
-
-    # Download Server Files
-    cd && git clone https://github.com/davidalvarez305/soflo_go.git
-
-    # Download SQL File
-    aws s3 cp ~/db/$DATABASE.sql ~/soflo_go/postgres/
-
-    # Copy SQL to Database
-    psql -h localhost -U $USER $DATABASE -f ~/soflo_go/postgres/$DATABASE.sql
-
-    # Start Server
-    cd soflo_go && sudo docker-compose -f docker-compose.yml up --build
+    echo "Initiating server setup..."
+    ssh -i $KEY_NAME ubuntu@$EC2_PUBLIC_ID "yes y | chmod +x ./prep/server.sh && sudo ./prep/server.sh $DATABASE $USER $S3_BUCKET"
 
 else
     echo "Missing either -d or -db or -u flag"
